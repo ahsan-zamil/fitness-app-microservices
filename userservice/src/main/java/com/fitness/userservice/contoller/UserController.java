3f9c6eac2b9574528b6f7272184e1051987e9534
Ahsan Zamil <ahsanzamil76@gmail.com>
2025-07-23 19:27:45 +0530

    userservice ms updated

--- userservice/src/main/java/com/fitness/userservice/contoller/UserController.java
+++ userservice/src/main/java/com/fitness/userservice/contoller/UserController.java
@@ -1,10 +1,11 @@
-package com.fitness.userservice.contoller;
+package com.fitness.userservice.controller;
 
 import com.fitness.userservice.dto.RegisterRequest;
 import com.fitness.userservice.dto.UserResponse;
 import com.fitness.userservice.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +25,8 @@ public class UserController {
     public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request){
         return ResponseEntity.ok(userService.register(request));
     }
+
+
     @GetMapping("/{userId}/validate")
     public ResponseEntity<Boolean> validateUser(@PathVariable String userId){
         return ResponseEntity.ok(userService.existByUserId(userId));
